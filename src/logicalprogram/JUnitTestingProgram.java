07388bf9de1b6bb372f655606ce212062ba8ff8c
Aakanksha <aakanksha0499@gmail.com>
2022-06-14 09:26:15 +0530

    Two Binary Program

--- src/logicalprogram/JUnitTestingProgram.java
+++ src/logicalprogram/JUnitTestingProgram.java
@@ -5,10 +5,29 @@ import java.util.Scanner;
 
 public class JUnitTestingProgram {
     public static void main(String[] args) {
-       binary(12);
+       twoBinary(12);
     }
 
-    static void binary(int i) {
-        System.out.println("Binary is " + Integer.toBinaryString(i));
-    }
+    static void twoBinary(int x) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1. Swap nibbles and find the new number\n" +
+                "2. Find the resultant number is the number is a power of 2\n");
+
+        System.out.println("\nEnter Choice: ");
+        System.out.println("Binary is " + Integer.toBinaryString(x));
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                int output = ((x & 0x0F) << 4 | (x & 0xF0) >> 4);
+                System.out.print(output);
+                break;
+
+            case 2:
+                if (x <= 0) {
+                    throw new IllegalArgumentException(x + " is too small.");
+                }
+                if ((x & -x) == x) {
+                    System.out.println("It is power of two");
+                }
+        }
 }
