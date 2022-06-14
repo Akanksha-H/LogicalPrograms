5c5d98968fb52d92e2d4538bc4f23e97a1b74270
Aakanksha <aakanksha0499@gmail.com>
2022-06-14 09:25:03 +0530

    Binary Of Number Program

--- src/logicalprogram/JUnitTestingProgram.java
+++ src/logicalprogram/JUnitTestingProgram.java
@@ -5,25 +5,10 @@ import java.util.Scanner;
 
 public class JUnitTestingProgram {
     public static void main(String[] args) {
-       squareRootUsingNewtonsMethod(12,1);
+       binary(12);
     }
 
-    static void squareRootUsingNewtonsMethod(double n, double l) {
-        double x = n;
-
-        // The closed guess will be stored in the root
-        double root;
-
-        // To count the number of iterations
-        int count = 0;
-
-        while (true) {
-            count++;
-            root = 0.5 * (x + (n / x));
-            if (Math.abs(root - x) < l)
-                break;
-            x = root;
-        }
-        System.out.println(root);
+    static void binary(int i) {
+        System.out.println("Binary is " + Integer.toBinaryString(i));
     }
 }
