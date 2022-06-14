5c033343513cbf8d1eb7ed55567bf2d615a8540c
Aakanksha <aakanksha0499@gmail.com>
2022-06-14 09:23:32 +0530

    Monthly Payment Program

--- src/logicalprogram/JUnitTestingProgram.java
+++ src/logicalprogram/JUnitTestingProgram.java
@@ -5,23 +5,30 @@ import java.util.Scanner;
 
 public class JUnitTestingProgram {
     public static void main(String[] args) {
-       temperatureConvertor();
+       monthlyPayment();
     }
-    static void temperatureConvertor(double value) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("1. Celcius to Fahrenheit\n" +
-                "2. Fahrenheit to Celcius\n");
-        System.out.println("\nEnter Choice: ");
-        int choice = sc.nextInt();
-        switch (choice) {
-            case 1:
-                value = (value * 9 / 5) + 32;
-                System.out.println("Celcius to Fahrenheit: " + value);
-                break;
-            case 2:
-                value = (value - 32) * 5 / 9;
-                System.out.println("Fahrenheit to Celcius: " + value);
-                break;
-        }
+    static double monthlyPayment() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter loan amount: ");
+        int loanAmount = scanner.nextInt();
+
+        System.out.print("Enter loan term (in years): ");
+        int termInYears = scanner.nextInt();
+
+        System.out.print("Enter interest rate: ");
+        double interestRate = scanner.nextDouble();
+        // Convert interest rate into a decimal
+        interestRate /= 100.0;
+
+        // Monthly interest rate is the yearly rate divided by 12
+        double monthlyRate = interestRate / 12.0;
+
+        // The length of the term in months is the number of years times 12
+        int termInMonths = termInYears * 12;
+        double monthlyPayment =
+                (loanAmount * monthlyRate) /
+                        (1 - Math.pow(1 + monthlyRate, -termInMonths));
+        System.out.println(monthlyPayment);
+        return monthlyPayment;
     }
 }
